5b7c4b7d2404075f6b4df43fcfbe3c9c6c45e9f6
Vasyl Mokhnatchuk <144223353+mokhnatchuk@users.noreply.github.com>
2026-05-10 01:01:27 +0300

    [PROMO-42] - fix Docker setup and SQLite tuning

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,9 +2,24 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./database.sqlite",
+      filename: "./data/database.sqlite",
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: function (connection, callback) {
+        connection.run("PRAGMA journal_mode=WAL", function () {
+          connection.run("PRAGMA busy_timeout=5000", function () {
+            connection.run("PRAGMA synchronous=NORMAL", function () {
+              connection.run("PRAGMA cache_size=-64000", function () {
+                connection.run("PRAGMA temp_store=MEMORY", function () {
+                  callback(null, connection);
+                });
+              });
+            });
+          });
+        });
+      },
+    },
     migrations: {
       directory: "./migrations",
     },
